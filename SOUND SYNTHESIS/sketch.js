c4b35c8bf7a5be4990346d0691f4cfac1d3c3bd0
WillDrac <willdrac@yahoo.com>
2023-08-05 17:05:47 -0400

    i give up

--- SOUND SYNTHESIS/sketch.js
+++ SOUND SYNTHESIS/sketch.js
@@ -104,22 +104,28 @@ function keyPressed() {
     }
 
     if (keys[keyCode]) {
-        if (thisBundle[keyCode] == undefined) { thisBundle[keyCode] = [] }
-        thisBundle[keyCode].push([snapTimeToBPM(timeSinceStart, bpm)])
+        let thisFreq = keys[keyCode][2]
+        if (thisBundle[thisFreq] == undefined) { thisBundle[thisFreq] = [] }
+        thisBundle[thisFreq].push([Math.round(bpm * timeSinceStart / 60)])
 
         previewPolysynth.noteAttack(keys[keyCode][1], 1)
     }
 
-    if (keyCode == 39) {
-        for (let i = 48; i <= 57; i++) {
-            if (keyIsDown(i)) {
-                let bundle = savedBundles[i]
-                for (let key in bundle) {
-                    for (let j = 0; j < bundle[key].length; j++) {
-                        bundle[key][j][0] = bundle[key][j][0] / 2
-                        bundle[key][j][1] = bundle[key][j][1] / 2
-                    }
-                }
+    // controlling aa bundle
+    for (let i = 48; i <= 57; i++) {
+        if (keyIsDown(i)) {
+            let bundle = savedBundles[i]
+            if (keyCode == 39) { //right arrow, speed up
+                savedBundles[i] = retimeBundle(bundle, 2)
+            }
+            if (keyCode == 37) { //left arrow, slow down
+                savedBundles[i] = retimeBundle(bundle, 1 / 2)
+            }
+            if (keyCode == 38) { //up arrow, pitch up
+                savedBundles[i] = pitchBundle(bundle, 23)
+            }
+            if (keyCode == 40) { //down arrow, pitch down
+                savedBundles[i] = pitchBundle(bundle, -23)
             }
         }
     }
@@ -127,17 +133,18 @@ function keyPressed() {
 
 function keyReleased() {
     if (keys[keyCode]) {
-        let thisTime = snapTimeToBPM(timeSinceStart, subBPM)
-        let thisArray = thisBundle[keyCode][thisBundle[keyCode].length - 1]
-        if (thisArray[0] >= thisTime) { thisTime = thisArray[0] + 60 / (bpm * 2) }
-        thisArray.push(thisTime)
+        let thisBeat = Math.round(bpm * timeSinceStart / 60)
+        let thisFreq = keys[keyCode][2]
+        let thisArray = thisBundle[thisFreq][thisBundle[thisFreq].length - 1]
+        if (thisArray[0] >= thisBeat) { thisBeat = thisArray[0] + 0.5 }
+        thisArray.push(thisBeat)
 
         previewPolysynth.noteRelease(keys[keyCode][1])
     }
 
-    // saving keys (number keys)
+    // saving keys (to number keys)
     if (keyCode >= 48 && keyCode <= 57) {
-        if (keyIsDown(83)) {//s
+        if (keyIsDown(83)) { //s
             savedBundles[keyCode] = thisBundle
         }
         else {
@@ -151,51 +158,89 @@ function snapTimeToBPM(time, bpm) {
     return Math.round(time / spb) * spb
 }
 
+// puts together multiple of the same note that are playing at the same time
+// *!make it so that the first beat is always 0
 function cleanUpBundle(bundle) {
     let output = {}
-    for (let keyNum in bundle) {
-        let key = bundle[keyNum]
-        output[keyNum] = []
+    for (let freq in bundle) {
+        let thisFrequency = bundle[freq]
+        output[freq] = []
 
         let currentStartTime = 0
         let latestEndTime = 0
-        for (let i = 0; i < key.length; i++) {
-            if (currentStartTime != key[i][0] && i != 0) { //new note, add the old one to output
-                output[keyNum].push([currentStartTime, latestEndTime])
+        for (let i = 0; i < thisFrequency.length; i++) {
+            if (currentStartTime != thisFrequency[i][0] && i != 0) { //new note, add the old one to output
+                output[freq].push([currentStartTime, latestEndTime])
                 latestEndTime = 0
             }
 
-            currentStartTime = key[i][0]
-            if (key[i][1] > latestEndTime) { latestEndTime = key[i][1] }
+            currentStartTime = thisFrequency[i][0]
+            if (thisFrequency[i][1] > latestEndTime) { latestEndTime = thisFrequency[i][1] }
         }
-        output[keyNum].push([currentStartTime, latestEndTime])
+        output[freq].push([currentStartTime, latestEndTime])
     }
 
     return output
 
 }
 
-const transitionTime = 0.3//might not be right
 function playBundle(bundle) {
     let oscillators = {}
-    for (let keyNum in bundle) {
-        oscillators[keyNum] = new p5.Oscillator(keys[keyNum][2], "sine")
-        let osc = oscillators[keyNum]
+    for (let frequency in bundle) {
+        oscillators[frequency] = new p5.Oscillator(float(frequency), "sine")
+        let osc = oscillators[frequency]
         osc.start()
         osc.amp(0, 0)
 
-        for (let i = 0; i < bundle[keyNum].length; i++) {
-            setTimeout(function () { osc.amp(1, transitionTime) }, bundle[keyNum][i][0] * 1000)
-            setTimeout(function () { osc.amp(0, transitionTime) }, bundle[keyNum][i][1] * 1000)
+        for (let i = 0; i < bundle[frequency].length; i++) {
+            playNote(
+                osc,
+                60 * (bundle[frequency][i][1] - bundle[frequency][i][0]) / bpm,
+                60 * bundle[frequency][i][0] / bpm
+            )
         }
     }
 
     return oscillators
 }
 
+function playNote(osc, timeHeld, timeUntilStart) {
+    if (osc == undefined) { osc = new p5.Oscillator("sine"); osc.start() }
+    if (timeUntilStart == undefined) { timeUntilStart = 0 }
+    const attackTime = 0.1
+    const decayTime = 0.3
+    const sustainLevel = 0.6
+    const releaseTime = 0.2
+    setTimeout(function () { osc.amp(1, attackTime) }, (timeUntilStart) * 1000)
+    setTimeout(function () { osc.amp(sustainLevel, decayTime) }, (timeUntilStart + attackTime) * 1000)
+    setTimeout(function () { osc.amp(0, releaseTime) }, (timeUntilStart + attackTime + decayTime + timeHeld - releaseTime) * 1000)
+}
+
+function retimeBundle(bundle, speedMultiplier) {
+    for (let key in bundle) {
+        for (let j = 0; j < bundle[key].length; j++) {
+            bundle[key][j][0] = bundle[key][j][0] / speedMultiplier
+            bundle[key][j][1] = bundle[key][j][1] / speedMultiplier
+        }
+    }
+    return bundle
+}
+
+function pitchBundle(bundle, frequencyChange) {
+    let newBundle = {}
+
+    for (let frequency in bundle) {
+        let newFrequency = float(frequency) + frequencyChange
+        newBundle[newFrequency] = bundle[frequency]
+    }
+
+    bundle = newBundle
+    return bundle
+}
+
 const keys = {
     81: ["q", "A4", 220],
-    87: ["w", "Bb4", 223.08],
+    87: ["w", "Bb4", 233.08],
     69: ["e", "B4", 246.94],
     82: ["r", "C4", 261.63],
     84: ["t", "Db4", 277.18],
@@ -207,24 +252,17 @@ const keys = {
     219: ["[", "G4", 392],
     221: ["]", "Ab5", 415.30]
 }
-//? b and m for pitch up and down
-
-// save bundle to number ~
-// select and speed/pitch bundle ~
-// ?preview plays unexpectedly after a while
-    //? dont rely on setTimeout
 
 // !plays infinitely if I stop the recording while holding a key
 
 // *rework replay to allow for sped up bundles
     // custom ADSR using a function to pass in time attack-release which sets up a few setTimeouts
 // *rework preview to not clip
-
-// * use beats instead of seconds in bundles
+// !still weird ^^^
 
 // *make a display for the calculated bpm instead of console.log
 
-// *to make it work with pitching up and down, bundles need to have notes listed under their pitch, not their key number
+// ?use something to get the frequencies of all the notes so that pitching up or down uses real notes
 
 // wrtuuyupopopopopo]opouopo
 //  https://music.youtube.com/watch?v=M_oobzkXcIs&list=PL0phUzFhhvnNnX_26ybcSK6ZDRgPM2JRL
