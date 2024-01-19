9e29dc0ee89d2d6b8deb2d02e90faca623b81e79
WillDrac <willdrac@yahoo.com>
2024-01-19 16:27:54 -0500

    genuary 19

--- Genuary 2024/19 - Flocking/boid.js
+++ Genuary 2024/19 - Flocking/boid.js
@@ -1,63 +1,97 @@
-class Boid{
-    constructor(pos, vel){
+class Boid {
+    constructor(pos, vel) {
         this.pos = pos
         this.vel = vel
         this.acc = createVector(0, 0)
-        this.maxSteering = 0.01
         this.maxSpeed = 2
     }
 
-    align(boids, dist){
+    align(boids, dist, strength) {
         let steering = createVector(0, 0)
         let boidsInDist = 0
         // make it check for boids on the other side of an edge by tiling the boids a lot of times 
-        for(let i=0;i<boids.length; i++){
+        for (let i = 0; i < boids.length; i++) {
             const d = this.pos.dist(boids[i].pos)
-            if (d < dist && boids[i]!==this){  
+            if (d < dist && boids[i] !== this) {
                 steering.add(boids[i].vel)
                 boidsInDist++
             }
         }
-        
-        if (boidsInDist > 0){
+
+        if (boidsInDist > 0) {
             steering.div(boidsInDist)
             steering.setMag(this.maxSpeed)
             // this.vel = this.vel.copy().add(steering).normalize()
             steering.sub(this.vel)
-            this.acc = steering.limit(this.maxSteering)
+            this.acc.add(steering.limit(strength))
         }
     }
 
-    cohesion(boids, dist){
+    cohesion(boids, dist, strength) {
         let steering = createVector(0, 0)
         let boidsInDist = 0
         // make it check for boids on the other side of an edge by tiling the boids a lot of times 
-        for(let i=0;i<boids.length; i++){
+        for (let i = 0; i < boids.length; i++) {
             const d = this.pos.dist(boids[i].pos)
-            if (d < dist && boids[i]!==this){  
-                steering.add(boids[i].position)
+            if (d < dist && boids[i] !== this) {
+                steering.add(boids[i].pos)
                 boidsInDist++
             }
         }
-        
-        if (boidsInDist > 0){
+
+        if (boidsInDist > 0) {
             steering.div(boidsInDist)
             steering.sub(this.pos)
             steering.setMag(this.maxSpeed)
             steering.sub(this.vel)
-            this.acc = steering.limit(this.maxSteering)
+            this.acc.add(steering.limit(strength))
         }
     }
 
-    show(){
-        circle(this.pos.x, this.pos.y, 5)
+    separation(boids, dist, strength) {
+        let steering = createVector(0, 0)
+        let boidsInDist = 0
+        // make it check for boids on the other side of an edge by tiling the boids a lot of times 
+        for (let i = 0; i < boids.length; i++) {
+            const d = this.pos.dist(boids[i].pos)
+            if (d < dist && boids[i] !== this) {
+                let difference = this.pos.copy().sub(boids[i].pos)
+                difference.div(d)
+                steering.add(difference)
+                boidsInDist++
+            }
+        }
+
+        if (boidsInDist > 0) {
+            steering.div(boidsInDist)
+            steering.setMag(this.maxSpeed)
+            steering.sub(this.vel)
+            this.acc.add(steering.limit(strength))
+        }
     }
 
-    update(){
-        this.vel.add(this.acc)
-        this.pos.add(this.vel)
+    evade(mousePos, dist, strength) {
+        const d = this.pos.dist(mousePos)
+        if (d < dist) {
+            this.acc.add(this.pos.copy().sub(mousePos).setMag((dist - d) / d ** 2).mult(d * strength))
+        }
+    }
 
-        this.pos.x = (this.pos.x+width*5) % width
-        this.pos.y = (this.pos.y+height*5) % height
+    show() {
+        push()
+        translate(this.pos.x, this.pos.y)
+        rotate(this.vel.heading() - PI / 2)
+        triangle(-3, -3, 3, -3, 0, 5)
+        pop()
+    }
+
+    update() {
+        this.vel.add(this.acc)
+        this.vel.limit(this.maxSpeed)
+        this.pos.add(this.vel)
+        this.acc.mult(0)
+
+        this.pos.x = (this.pos.x + width * 5) % width
+        this.pos.y = (this.pos.y + height * 5) % height
     }
 }
--- Genuary 2024/19 - Flocking/sketch.js
+++ Genuary 2024/19 - Flocking/sketch.js
@@ -1,20 +1,26 @@
 let flock = []
 
 function setup() {
-  createCanvas(600, 400);
+    createCanvas(600, 400)
 
-  for(let i=0;i<50;i++){
-    flock.push(new Boid(createVector(random(width), random(height)), p5.Vector.random2D()))
-  }
+    for (let i = 0; i < 100; i++) {
+        flock.push(new Boid(createVector(random(width), random(height)), p5.Vector.random2D()))
+    }
+
+    noStroke()
 }
 
 function draw() {
-  background(220);
+    background(51)
 
-  for (let boid of flock){
-    // boid.align(flock, 50)
-    boid.cohesion(flock, 50)
-    boid.update()
-    boid.show()
-  }
+    for (let boid of flock) {
+        boid.align(flock, 50, 0.05)
+        boid.cohesion(flock, 50, 0.06)
+        boid.separation(flock, 50, 0.055)
+        boid.evade(createVector(mouseX, mouseY), 100, 1)
+    }
+    for (let boid of flock) {
+        boid.update()
+        boid.show()
+    }
 }
