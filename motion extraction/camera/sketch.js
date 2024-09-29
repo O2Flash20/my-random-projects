ea3e183420ce47c26cb7645e9aeae8e05efb12e0
WillDrac <willdrac@yahoo.com>
2024-09-29 10:38:49 -0400

    made the effect look much better and work at a higher frame rate

--- motion extraction/camera/sketch.js
+++ motion extraction/camera/sketch.js
@@ -1,4 +1,17 @@
-let v = document.getElementById("inputVideo")
+let cameraStream
+let filters
+function setup() {
+    noCanvas()
+    cameraStream = createCapture({
+        audio: false,
+        video: {
+            facingMode: "environment"
+        }
+    })
+    cameraStream.hide()
+
+    filters = "contrast(1.01) brightness(20) blur(" + (Math.max(cameraStream.width, cameraStream.height) / 1920) * 5 + "px)"
+}
 
 let cI = document.getElementById("inputCanvas")
 let ctxI = cI.getContext("2d")
@@ -6,7 +19,9 @@ let ctxI = cI.getContext("2d")
 let c = document.getElementById("compositeCanvas")
 let ctx = c.getContext("2d")
 
-const filters = "brightness(5) contrast(2) brightness(20) blur(5px)"
+let ca = document.getElementById("accumulatorCanvas")
+let ctxa = ca.getContext("2d")
+
 let cf = document.getElementById("filterCanvas")
 let ctxf = cf.getContext("2d")
 
@@ -17,12 +32,12 @@ oldWidth = 0
 oldHeight = 0
 setInterval(function () {
     if (oldWidth !== cameraStream.elt.width) {
-        cI.width = c.width = cf.width = cF.width = cameraStream.elt.width
+        cI.width = c.width = ca.width = cf.width = cF.width = cameraStream.elt.width
         ctxf.filter = filters
     }
     oldWidth = cameraStream.elt.width
     if (oldHeight !== cameraStream.elt.height) {
-        cI.height = c.height = cf.height = cF.height = cameraStream.elt.height
+        cI.height = c.height = ca.height = cf.height = cF.height = cameraStream.elt.height
         ctxf.filter = filters
     }
     oldHeight = cameraStream.elt.height
@@ -33,23 +48,13 @@ setInterval(function () {
     ctx.globalCompositeOperation = 'difference'
     ctx.drawImage(cI, 0, 0) //now put the new one on, now with the difference blend mode on to only get what changed
 
-    ctxf.drawImage(c, 0, 0) //apply the filters on all that (blur and brightness to make it shine)
+    ctxa.globalAlpha = 0.2
+    ctxa.drawImage(c, 0, 0)
+
+    ctxf.drawImage(ca, 0, 0) //apply the filters on all that (blur and brightness to make it shine)
 
     ctxF.globalCompositeOperation = 'source-over' //stick it on top of the regular screen
     ctxF.drawImage(cameraStream.elt, 0, 0)
     ctxF.globalCompositeOperation = 'screen'
     ctxF.drawImage(cf, 0, 0)
-}, 100)
-
-let cameraStream
-function setup(){
-    noCanvas()
-    cameraStream = createCapture({audio: false,
-        video: {
-            facingMode: "environment"
-        }
-    })
-    cameraStream.hide()
-
-    console.log()
-}
+}, 16)
