c7d661ee13a89b49fe822bd04639f72a5d6e5382
WillDrac <willdrac@yahoo.com>
2024-09-29 10:18:26 -0400

    maybe fixed in on phones?

--- motion extraction/camera/sketch.js
+++ motion extraction/camera/sketch.js
@@ -13,44 +13,43 @@ let ctxf = cf.getContext("2d")
 let cF = document.getElementById("finalCanvas")
 let ctxF = cF.getContext("2d")
 
-navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: {
-        facingMode: "environment"
-    }
-})
-    .then(function (mediaStream) {
-        v.srcObject = mediaStream
-        v.onloadedmetadata = function (e) {
-            v.play()
-        }
-    })
-    .catch(function (err) { console.log(err.name + ": " + err.message) })
-
 oldWidth = 0
 oldHeight = 0
 setInterval(function () {
-    if (oldWidth !== v.videoWidth) {
-        v.width = cI.width = c.width = cf.width = cF.width = v.videoWidth
+    if (oldWidth !== cameraStream.elt.width) {
+        cI.width = c.width = cf.width = cF.width = cameraStream.elt.width
         ctxf.filter = filters
     }
-    oldWidth = v.videoWidth
-    if (oldHeight !== v.videoHeight) {
-        v.height = cI.height = c.height = cf.height = cF.height = v.videoHeight
+    oldWidth = cameraStream.elt.width
+    if (oldHeight !== cameraStream.elt.height) {
+        cI.height = c.height = cf.height = cF.height = cameraStream.elt.height
         ctxf.filter = filters
     }
-    oldHeight = v.videoHeight
+    oldHeight = cameraStream.elt.height
 
     ctx.globalCompositeOperation = 'source-over'
     ctx.drawImage(cI, 0, 0) //put the old video on the screen
-    ctxI.drawImage(v, 0, 0) //update the video
+    ctxI.drawImage(cameraStream.elt, 0, 0) //update the video
     ctx.globalCompositeOperation = 'difference'
     ctx.drawImage(cI, 0, 0) //now put the new one on, now with the difference blend mode on to only get what changed
 
     ctxf.drawImage(c, 0, 0) //apply the filters on all that (blur and brightness to make it shine)
 
     ctxF.globalCompositeOperation = 'source-over' //stick it on top of the regular screen
-    ctxF.drawImage(v, 0, 0)
+    ctxF.drawImage(cameraStream.elt, 0, 0)
     ctxF.globalCompositeOperation = 'screen'
     ctxF.drawImage(cf, 0, 0)
 }, 100)
+
+let cameraStream
+function setup(){
+    noCanvas()
+    cameraStream = createCapture({audio: false,
+        video: {
+            facingMode: "environment"
+        }
+    })
+    cameraStream.hide()
+
+    console.log()
+}
