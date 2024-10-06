269a6d29915e2b81638e8451e31a2d8cca2b5daa
WillDrac <willdrac@yahoo.com>
2024-10-06 17:27:07 -0400

    done all the hard math, just need a gui now

--- polynomial solver/script.js
+++ polynomial solver/script.js
@@ -17,8 +17,8 @@ class Matrix {
 
     multiplyScalar(scalar) {
         let multiplied = new Matrix(this.rows, this.columns)
-        for (let i = 0; i < this.rows; i++){
-            for (let j = 0; j < this.columns; j++){
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.columns; j++) {
                 multiplied.entries[i][j] = this.entries[i][j] * scalar
             }
         }
@@ -26,7 +26,7 @@ class Matrix {
         return multiplied
     }
 
-    determinant(dontIncludeEntry) { //when finding the cofactor, the entry isn't included in the determinant (yeah, that means it's not actually a determinant)
+    determinant() {
         // if the number of rows and columns is 2, just get the number the quick way
         if (this.rows == 2 && this.columns == 2) {
             return this.entries[0][0] * this.entries[1][1] - this.entries[0][1] * this.entries[1][0]
@@ -51,12 +51,7 @@ class Matrix {
             minorMatrix.entries = minorEntries
             const minor = minorMatrix.determinant(false)
 
-            if (dontIncludeEntry) {
-                solution += sign * minor
-            }
-            else {
-                solution += entry * sign * minor
-            }
+            solution += entry * sign * minor
         }
 
         return solution
@@ -68,24 +63,23 @@ class Matrix {
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.columns; j++) {
 
-                let minorMatrix = new Matrix(this.rows-1, this.columns-1)
+                let minorMatrix = new Matrix(this.rows - 1, this.columns - 1)
                 let minorMatrixEntries = []
                 for (let k = 0; k < this.rows - 1; k++) {
                     let thisRow = []
                     for (let l = 0; l < this.columns - 1; l++) {
-                        const rowIndex = k < i ? k : k + 1; const columnIndex = l < j ? l : l + 1;
+                        const rowIndex = k < i ? k : k + 1; const columnIndex = l < j ? l : l + 1
                         thisRow.push(
                             this.entries
                             [rowIndex]
                             [columnIndex]
-                            * (-1)**(rowIndex+1+columnIndex+1)
                         )
                     }
                     minorMatrixEntries.push(thisRow)
                 }
                 minorMatrix.entries = minorMatrixEntries
 
-                cofactor.entries[i][j] = minorMatrix.determinant(true)
+                cofactor.entries[i][j] = minorMatrix.determinant() * (-1) ** (i + 1 + j + 1)
             }
         }
 
@@ -94,8 +88,8 @@ class Matrix {
 
     transpose() {
         let transpose = new Matrix(this.rows, this.columns)
-        for (let i = 0; i < this.rows; i++){
-            for (let j = 0; j < this.columns; j++){
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.columns; j++) {
                 transpose.entries[i][j] = this.entries[j][i]
             }
         }
@@ -107,6 +101,47 @@ class Matrix {
     }
 
     inverse() {
-        return this.adjoint().multiplyScalar(1/this.determinant())
+        return this.adjoint().multiplyScalar(1 / this.determinant())
     }
+
+    multiplyVector(vector) {
+        if (this.columns !== vector.rows) { console.log("Matrix multiplication not gonna workie") }
+        let product = new Matrix(vector.rows, 1)
+        product.entries[0][0] = 0 //make it be all 0s to start
+
+        for (let i = 0; i < vector.rows; i++) {
+            for (let j = 0; j < vector.rows; j++) {
+                product.entries[i][0] += vector.entries[j][0] * this.entries[i][j]
+            }
+        }
+
+        return product
+    }
+}
+
+// pointsArray: [{isDerivative, x, y}, {isDerivative, x, y}, ...]
+// Ax = b => x = A(^-1) b
+function pointsToPolynomialParameters(pointsArray) {
+    let A = new Matrix(pointsArray.length, pointsArray.length)
+    let b = new Matrix(pointsArray.length, 1)
+
+    // set up the b vector
+    for (let i = 0; i < b.rows; i++) {
+        b.entries[i][0] = pointsArray[i].y
+    }
+
+    // set up the A matrix
+    for (let i = 0; i < A.rows; i++) {
+        for (let j = 0; j < A.columns; j++) {
+            const k = A.columns - 1 - j
+            if (pointsArray[i].isDerivative) {
+                k == 0 ? A.entries[i][j] = 0 : A.entries[i][j] = k * pointsArray[i].x ** (k - 1)
+            }
+            else {
+                A.entries[i][j] = pointsArray[i].x ** k
+            }
+        }
+    }
+
+    return A.inverse().multiplyVector(b)
 }
