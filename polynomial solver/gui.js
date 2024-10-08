370c55ac52daf69905ff9a1ce8e8d7627a9b3c1c
WillDrac <willdrac@yahoo.com>
2024-10-07 21:31:38 -0400

    finished graph visuals, started adding points that can be controlled

--- polynomial solver/gui.js
+++ polynomial solver/gui.js
@@ -4,11 +4,12 @@ let scale = 100 //pixels per unit on the plane
 let mouseIsDragging = false
 let mousePos = { x: 0, y: 0 }
 
+let pointsInfoDiv
+
 function setup() {
     createCanvas(1000, 600)
     stroke("white")
     textSize(20)
-    textAlign(CENTER, TOP)
 
     canvas.addEventListener('wheel', function (event) {
         const scaleMultiplier = 1 - event.deltaY / 1000
@@ -26,6 +27,9 @@ function setup() {
     canvas.addEventListener("mouseup", function (event) {
         mouseIsDragging = false
     })
+    canvas.addEventListener("mouseleave", function (event) {
+        mouseIsDragging = false
+    })
     canvas.addEventListener("mousemove", function (event) {
         const mouseDp = { x: event.clientX - mousePos.x, y: event.clientY - mousePos.y }
         mousePos = { x: event.clientX, y: event.clientY }
@@ -33,11 +37,14 @@ function setup() {
             originPos = { x: originPos.x + mouseDp.x, y: originPos.y + mouseDp.y }
         }
     })
+
+    pointsInfoDiv = createElement("div")
 }
 
 function draw() {
     background(25)
 
+    stroke(255)
     strokeWeight(7)
     line(originPos.x, 0, originPos.x, height)
     line(0, originPos.y, width, originPos.y)
@@ -45,23 +52,56 @@ function draw() {
     const numbersInterval = width / (scale * 10)
     const gridScale = nearestNiceNumber(numbersInterval)
 
+    const numDecimals = gridScale >= 1 ? 0 : Math.max(0, -Math.floor(Math.log10(gridScale))) //the number of decimal places in the text showing the scale
+
     strokeWeight(2)
+    fill(255)
     const lineSpacing = gridScale * scale // spacing in pixels
 
+    textAlign(CENTER, TOP)
     const verticalLinesUntilOrigin = originPos.x / lineSpacing
     const firstVerticalLine = verticalLinesUntilOrigin % 1 * lineSpacing
-    const numberVerticalLines = Math.ceil(width / lineSpacing)
+    const numberVerticalLines = Math.ceil(width / lineSpacing) + 1
     for (let i = 0; i < numberVerticalLines; i++) {
+        const label = Math.ceil(i - verticalLinesUntilOrigin) * gridScale
+
+        strokeWeight(2)
+        stroke(255)
         line(firstVerticalLine + i * gridScale * scale, 0, firstVerticalLine + i * gridScale * scale, height)
-        const label = Math.ceil(i-verticalLinesUntilOrigin)*gridScale
-        text(label, firstVerticalLine + i * gridScale * scale, originPos.y)
+
+
+        strokeWeight(5)
+        stroke(25)
+        text(label == 0 ? 0 : label.toFixed(numDecimals), firstVerticalLine + i * gridScale * scale, originPos.y + 5)
     }
 
-    const horizontalLinesUntilOrigin = originPos.y/lineSpacing
-    const firstHorizontalLine = horizontalLinesUntilOrigin%1*lineSpacing
-    const numberHorizontalLines = Math.ceil(height/lineSpacing)
+    textAlign(LEFT, CENTER)
+    const horizontalLinesUntilOrigin = originPos.y / lineSpacing
+    const firstHorizontalLine = horizontalLinesUntilOrigin % 1 * lineSpacing
+    const numberHorizontalLines = Math.ceil(height / lineSpacing) + 1
     for (let i = 0; i < numberHorizontalLines; i++) {
+        const label = -Math.ceil(i - horizontalLinesUntilOrigin) * gridScale
+
+        strokeWeight(2)
+        stroke(255)
         line(0, firstHorizontalLine + i * gridScale * scale, width, firstHorizontalLine + i * gridScale * scale)
+
+
+        if (label !== 0) {
+            strokeWeight(5)
+            stroke(25)
+            text(label.toFixed(numDecimals), originPos.x + 5, firstHorizontalLine + i * gridScale * scale)
+        }
+    }
+
+    // render points
+    noStroke()
+    for (let i = 0; i < points.length; i++) {
+        const p = points[i]
+        strokeWeight(3)
+        stroke(255)
+        fill(0)
+        ellipse(p.pos.x * scale + originPos.x, -p.pos.y * scale + originPos.y, 10)
     }
 
 }
@@ -71,3 +111,61 @@ function nearestNiceNumber(number) {
 
     return Math.round(number / (10 ** place)) * (10 ** place)
 }
+
+let points = []
+class Point {
+    constructor(x, y) {
+        this.pos = { x: x, y: y }
+        this.slope = undefined
+    }
+}
+
+function addPoint() {
+    points.push(new Point((mouseX - originPos.x) / scale, -(mouseY - originPos.y) / scale))
+}
+
+// turns the Point class into an array that's format for the polynomial solver
+function formatPoints() {
+    let output = []
+
+    for (let i = 0; i < points.length; i++) {
+        const p = points[i]
+        output.push({ isDerivative: false, x: p.pos.x, y: p.pos.y })
+        if (p.slope) {
+            output.push({ isDerivative: true, x: p.pos.x, y: p.pos.y })
+        }
+    }
+
+    return output
+}
+
+document.addEventListener("keypress", function (event) {
+    const k = event.key
+
+    if (k == "c") {
+        let isNearPoint = false
+
+        for (let i = 0; i < points.length; i++) {
+            const p = points[i]
+            const pointScreenPos = { x: p.pos.x * scale + originPos.x, y: -p.pos.y * scale + originPos.y }
+            const distToPoint = Math.sqrt((pointScreenPos.x - mouseX) ** 2 + (pointScreenPos.y - mouseY) ** 2)
+            if (distToPoint < 20) {
+                points.splice(i, 1)
+                isNearPoint = true
+            }
+        }
+
+        if (!isNearPoint) {
+            addPoint()
+        }
+
+    }
+})
+
+/*
+TODO:
+be able to drag points
+make inputs for the exact coordinates of the points
+have a handle for the slope at each points
+compute the graph very often and draw it each frame
+*/
